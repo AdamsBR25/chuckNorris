e30e466fcfce4892d5a355ec9ae839ab01584e45
Brady Adams <bradyryanadams@gmail.com>
2022-04-25 15:26:27 -0400

    Initial Commit

---
+++ Shared/The_Red_AllianceApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Red_AllianceApp.swift
+//  Shared
+//
+//  Created by Brady Adams on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Red_AllianceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
