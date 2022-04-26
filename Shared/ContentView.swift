85d9a5e0043c2a47fa3cb47e8c8bba8e470d4d75
Brady Adams <bradyryanadams@gmail.com>
2022-04-25 22:18:08 -0400

    add app icons and notifications

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,27 +7,96 @@
 
 import Foundation
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     @State private var joke: String = ""
+    private var jokeArray: [String] = []
+    
+    func wisdom() async -> String {
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.chucknorris.io/jokes/random")!)
+            let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
+            joke = decodedResponse?.value ?? ""
+            return joke
+            
+        } catch {
+            print("error in wisdom fetch")
+        }
+        return ""
+    }
+    
+    func notification() async {
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Daily Chuck Norris Wisdom"
+        notificationContent.body = await wisdom()
+        notificationContent.badge = NSNumber(value: 1)
+        notificationContent.sound = .default
+        notificationContent.categoryIdentifier = "wisdom.reminder.category"
+                        
+        var datComp = DateComponents()
+        datComp.hour = 7
+        datComp.minute = 20
+        let trigger = UNCalendarNotificationTrigger(dateMatching: datComp, repeats: true)
+        let request = UNNotificationRequest(identifier: "wisdom", content: notificationContent, trigger: trigger)
+                        UNUserNotificationCenter.current().add(request) { (error : Error?) in
+                            if let theError = error {
+                                print(theError.localizedDescription)
+                            }
+                        }
+    }
+    
+    func stopNotification() async {
+        let notifCenter = UNUserNotificationCenter.current()
+        notifCenter.removePendingNotificationRequests(withIdentifiers: ["wisdom"])}
+    
+    func setCategories() {
+        let clearRepeatAction = UNNotificationAction(
+           identifier: "clear.repeat.action",
+           title: "Stop Repeat",
+           options: [])
+        
+        let wisdomCategory = UNNotificationCategory(
+            identifier: "wisdom.reminder.category",
+            actions: [clearRepeatAction],
+            intentIdentifiers: [],
+            options: [])
+        
+        UNUserNotificationCenter.current().setNotificationCategories([wisdomCategory])
+        }
+        
     var body: some View {
         VStack{
+            HStack {
+                Button {
+                    Task {
+                        
+                    }
+                } label: {
+                    Text("Enable Notifications")
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button {
+                    Task {
+                        
+                    }
+                } label: {
+                    Text("Disable Notifications")
+                }
+                .padding()
+            }
+            
             Spacer()
             Text(joke)
                 .padding(20)
             Spacer()
             Button {
                 Task {
-                    if #available(iOS 15.0, *) {
-                        let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.chucknorris.io/jokes/random")!)
-                        let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
-                        joke = decodedResponse?.value ?? ""
-                    } else {
-//                        let (data, _) = URLSession.shared.UR(with: URL(string: "https://api.chucknorris.io/jokes/random")!)
-//                        let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
-//                        joke = decodedResponse?.value ?? ""
-                    }
-                    
+                   joke = await wisdom()
                 }
             } label: {
                 Text("Fetch Joke")
--- Shared/The_Red_AllianceApp.swift
+++ Shared/The_Red_AllianceApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct The_Red_AllianceApp: App {
+    @State public var notifAllowed = false
+    init() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            
+            if let error = error {
+                print(error)
+            }
+            
+            // Enable or disable features based on the authorization.
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
